1340f430e819ac091358201ff4abcb8165011cc6
suman98 <41943773+suman98@users.noreply.github.com>
2024-02-08 15:24:57 +0545

    updates

--- task1/views.py
+++ task1/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from myapp.services.scrapper import crawl_and_index, search
+from task1.services.scrapper import crawl_and_index, search
 base_url = "https://pureportal.coventry.ac.uk/en/organisations/centre-for-health-and-life-sciences"
 index_path = "storage"
 
