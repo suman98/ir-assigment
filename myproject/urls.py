a380191278d1b499e0e4e7daf7b49a9e754e2719
suman98 <41943773+suman98@users.noreply.github.com>
2024-02-09 21:07:45 +0545

    fixed base_url

--- myproject/urls.py
+++ myproject/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('myapp.urls')),
+    path('', include('task1.urls')),
     path('admin/', admin.site.urls),
 
 ]
--- task1/views.py
+++ task1/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from task1.services.scrapper import crawl_and_index, search
-base_url = "https://pureportal.coventry.ac.uk/en/organisations/centre-for-health-and-life-sciences"
+base_url = "https://pureportal.coventry.ac.uk/en/organisations/ihw-centre-for-health-and-life-sciences-chls"
 index_path = "storage"
 
 def hello_world(request):
@@ -15,3 +15,4 @@ def searchQuery(request):
     query = request.GET.get('query')
     response = search(query, index_path)
     return JsonResponse(response)
+
