c6755fe1820f15596b8aafabd51e8ded463a5948
suman98 <41943773+suman98@users.noreply.github.com>
2024-02-12 13:22:12 +0545

    code formatting

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from task1.services.scrapper import crawl_and_index, search
+from myapp.services.scrapper import crawl_and_index, search
 base_url = "https://pureportal.coventry.ac.uk/en/organisations/ihw-centre-for-health-and-life-sciences-chls"
 index_path = "storage"
 
